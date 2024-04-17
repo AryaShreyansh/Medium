8f4588a6402fe0de9746c535dd84564ecbf0f61b
Shreyansh Arya <shreyanshcse@gmail.com>
2024-04-17 08:57:53 +0530

    added a logout button

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -8,16 +8,26 @@ export const Appbar = ()=>{
                  Medium
             </Link>
             
-
-            <div>
-                <Link to={`/publish`} >
-                <button type="button" className="text-white bg-green-700
-                 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300
-                  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-6
-                    me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700
-                    dark:focus:ring-green-800">New</button>
-                </Link>    
-                <Avatar name={"Shreyansh"} size={"big"}/>
+            <div className="flex">
+                <div className="mt-1">
+                    <Link to={`/publish`} >
+                    <button type="button" className="text-white bg-green-700
+                    hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300
+                    font-medium rounded-full text-sm px-5 py-2.5 text-center mr-6
+                        me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700
+                        dark:focus:ring-green-800">New</button>
+                    </Link>  
+                </div>
+                <div>
+                    <Link to={`/signin`} >
+                    <button type="button" className="text-white bg-green-700
+                    hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300
+                    font-medium rounded-full text-sm px-5 py-2.5 text-center mr-6
+                        me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700
+                        dark:focus:ring-green-800">Logout</button>
+                    </Link>    
+                    <Avatar name={"Shreyansh"} size={"big"}/>
+                </div>
             </div>
         </div>
     )
